9222f7101e8c28fe563a34e32c09e17300f92028
kokopi122 <99959897+kokopi122@users.noreply.github.com>
2024-05-30 00:57:16 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,104 +1,197 @@
-let tg = window.Telegram.WebApp;
+const cookie = document.querySelector("#cookie");
+const autoClick = document.querySelector("#auto-click");
+const autoClickTextPrice = document.querySelector("#auto-click .price span");
+const upgradeClick = document.querySelector("#upgrade-click");
+const upgradeClickTextPrice = document.querySelector("#upgrade-click .price span");
 
-tg.expand();
+const updateScore = cookies => {
+    const title = document.querySelector("title");
+    const score = document.querySelector("#score span");
 
-tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = '#2cab37';
+    score.innerText = cookies;
+    title.innerHTML = cookies + " cookies - Cookie Clicker"
 
-let item = "";
+    localStorage.setItem("cookies", cookies);
+}
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+const updatePowerupsStorage = powerup => {
+    let powerups = JSON.parse(localStorage.getItem("powerups")) || [];
+    powerups.push(powerup);
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+    localStorage.setItem("powerups", JSON.stringify(powerups));
+}
+
+const getStorage = () => {
+    const cookies = localStorage.getItem("cookies") || 0;
+    const powerups = JSON.parse(localStorage.getItem("powerups")) || [];
+
+    const storage = {
+        "cookies": cookies,
+        "powerups": powerups
+    }
+
+    return storage;
+}
+
+const cookieClicked = cookies => {
+    const storage = getStorage();
+
+    const score = document.querySelector("#score span");
+    const scoreValue = cookies ? cookies : parseInt(score.innerText);
+
+    let newScore;
+
+    if(storage.powerups.includes("upgrade-click")) {
+        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length;
+        if(multiplier == 1){
+            newScore = scoreValue + 2;
+        } else {
+            newScore = scoreValue + (2 ** multiplier)
+        }
+    } else {
+        newScore = scoreValue + 1;
+    }
+
+    updateScore(newScore);
+}
+
+const createParticle = (x,y) => {
+    const cookieClicks = document.querySelector(".cookie-clicks");
+
+    const particle = document.createElement("img");
+    particle.setAttribute("src", "img/cookie.png");
+    particle.setAttribute("class", "cookie-particle");
+    particle.style.left = x + "px";
+    particle.style.top = y + "px";
+
+    cookieClicks.appendChild(particle);
+
+
+    setTimeout(() => {
+        cookieClicks.removeChild(particle);
+    }, 3000);
+}
+
+cookie.addEventListener("click", (e) => {
+    createParticle(e.clientX, e.clientY);
+    cookieClicked()
 });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
+const autoClickCookie = () => {
+    setInterval(() => {
+        const score = document.querySelector("#score span");
+        const scoreValue = parseInt(score.innerText);
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
+        newScore = scoreValue + 1;
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
+        updateScore(newScore);
+    }, 1000)
+}
 
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
+autoClick.addEventListener("click", () => {
+    const price = autoClick.getAttribute("data-price");
+    const score = document.querySelector("#score span");
+    const scoreValue = parseInt(score.innerText)
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
+    if (scoreValue >= price) {
+        updatePowerupsStorage("auto-click");
 
+        const storage = getStorage();
+        const quantAutoClicks = storage.powerups.filter(powerup => powerup == "auto-click").length;
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+        const newScore = scoreValue - price;
 
+        updateScore(newScore)
 
-let usercard = document.getElementById("usercard");
+        if(quantAutoClicks == 1) {
+            autoClick.setAttribute("data-price", 100 * 2);
+            autoClickTextPrice.innerHTML = 100 * 2;
+        } else {
+            autoClick.setAttribute("data-price", 100 * (quantAutoClicks + 1));
+            autoClickTextPrice.innerHTML = 100 * (quantAutoClicks + 1);
+        }
 
-let p = document.createElement("p");
+        document.querySelector(".auto-clicks").classList.remove("disable");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+        document.querySelector(".auto-clicks .cursors").innerHTML += '<img src="img/cursor.png" alt="cursor" id="cursor" class="cursor auto">'
 
-usercard.appendChild(p);
+        autoClickCookie();
+    } else {
+        autoClick.classList.add("invalid")
+        setTimeout(() => {
+            autoClick.classList.remove("invalid")
+        }, 300);
+    }
+})
 
+upgradeClick.addEventListener("click", () => {
+    const price = upgradeClick.getAttribute("data-price");
+    const score = document.querySelector("#score span");
+    const scoreValue = parseInt(score.innerText)
 
+    if (scoreValue >= price) {
+        updatePowerupsStorage("upgrade-click");
 
+        const storage = getStorage();
+        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length;
 
+        const newScore = scoreValue - price;
 
+        updateScore(newScore)
 
+        if(multiplier == 1) {
+            upgradeClick.setAttribute("data-price", 100 * 2);
+            upgradeClickTextPrice.innerHTML = 100 * 2;
+        } else {
+            upgradeClick.setAttribute("data-price", 100 * (2 ** multiplier));
+            upgradeClickTextPrice.innerHTML = 100 * (2 ** multiplier);
+        }
+    } else {
+        upgradeClick.classList.add("invalid")
+        setTimeout(() => {
+            upgradeClick.classList.remove("invalid")
+        }, 300);
+    }
+})
 
+const getSavedData = () => {
+    const storage = getStorage();
 
+    updateScore(storage.cookies);
+
+    if (storage.powerups.includes("upgrade-click")) {
+        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length;
+
+        if(multiplier == 1) {
+            upgradeClick.setAttribute("data-price", 100 * 2);
+            upgradeClickTextPrice.innerHTML = 100 * 2;
+        } else {
+            upgradeClick.setAttribute("data-price", 100 * (2 ** multiplier));
+            upgradeClickTextPrice.innerHTML = 100 * (2 ** multiplier);
+        }
+    }
+
+    if(storage.powerups.includes("auto-click")) {
+        const quantAutoClicks = storage.powerups.filter(powerup => powerup == "auto-click").length;
+
+        document.querySelector(".auto-clicks").classList.remove("disable")
+
+        if(quantAutoClicks == 1) {
+            autoClick.setAttribute("data-price", 100 * 2);
+            autoClickTextPrice.innerHTML = 100 * 2;
+        } else {
+            autoClick.setAttribute("data-price", 100 * (quantAutoClicks + 1));
+            autoClickTextPrice.innerHTML = 100 * (quantAutoClicks + 1);
+        }
+
+        for (i=1;i <= quantAutoClicks; i++) {
+            autoClickCookie();
+
+            document.querySelector(".auto-clicks").classList.remove("disable");
+            document.querySelector(".auto-clicks .cursors").innerHTML += '<img src="img/cursor.png" alt="cursor" id="cursor" class="cursor auto">'
+
+        }
+    }
+}
+
+document.addEventListener("load", getSavedData());
